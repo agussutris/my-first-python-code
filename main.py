9471a803b4918e408086048e33baa81a487eac00
agussutris <agus.sutrisno03@gmail.com>
2023-02-27 14:01:05 +0700

    My first python code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Semua sintaksi dasar bahsa pemograman terdiri dari :
+Semua sintaksi dasar bahasa pemograman terdiri dari :
 1. Sekuensial: langkah berurutan
 2. Percabangan: langkah melompat jika kondisi terpenuhi
 3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
@@ -9,7 +9,7 @@ Semua sintaksi dasar bahsa pemograman terdiri dari :
 
 print('Ibu berkata, "Pergi ke toko"')
 print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
-print('Ibu menjawab, Beli satu botol susu, dan jika ada telor beli 6"')
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
 print("Dan mulai berbelanja")
 
 
