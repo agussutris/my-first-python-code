4cf387696a742a4a66629871a1ec7df43545529a
agussutris <agus.sutrisno03@gmail.com>
2023-02-27 11:31:20 +0700

    My first python code

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 """
-ini adalah demo project pertama dengan python
+Semua sintaksi dasar bahsa pemograman terdiri dari :
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
-
-print("hello world!")
-print("Agus Sutrisno")
+# Sekuensial
 
 
-
-
-print("Dwianatalia")
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, Beli satu botol susu, dan jika ada telor beli 6"')
+print("Dan mulai berbelanja")
 
 
 
